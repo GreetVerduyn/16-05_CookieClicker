9928d0f63c6f154ac28aad4d19b70292caad79ef
Greet Verduyn <greetverduyn@proximus.be>
2022-05-16 14:20:45 +0200

    display multiplier counter + add text in read me

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 16-05_CookieClicker
+
+I used Bootstraps:  * to split the screen in 3 parts
+                    * to make the buttons
+
--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@
 // counter button
     let count = 0;
     let amountToAdd = 1;
-    let priceMultiplier = 500;
+    let priceMultiplier = 10;
 
     let counterButton = document.getElementById("counter");
     let multiplierButton = document.getElementById("mulitplier");
 
     counterButton.innerHTML = count.toString();
-    multiplierButton.innerHTML = "x";
-
+    multiplierButton.innerHTML =  ` by multiplier (${priceMultiplier} cookies):
+    click to have ${amountToAdd *2} cookies per click instead of ${amountToAdd} cookie per click`;
 
     counterButton.addEventListener("click", function(){
         count= count+ amountToAdd;
@@ -18,10 +18,14 @@
     });
 
     multiplierButton.addEventListener("click", function(){
-        if (count>priceMultiplier){
+        if (count>=priceMultiplier){
             amountToAdd= amountToAdd*2;
             counterButton.innerHTML = count - priceMultiplier;
-            count=count - priceMultiplier
+            count = count - priceMultiplier;
+            multiplierButton.innerHTML = `by multiplier (${priceMultiplier} cookies):
+            click to have ${amountToAdd *2} cookies per click instead of ${amountToAdd} cookie per click`;
+
+
         } else {
             alert("you don't have enough cookies to buy this")
         }
